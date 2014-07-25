4a009bc01119b5641d330d669b81ce7a584f4413
dumbbird <dumbbird0414@hotmail.com>
2014-07-25 14:58:45 +0800

    ver 1.8.3

--- Collections-off.js
+++ Collections-off.js
@@ -1,5 +1,6 @@
 $(function () {
 	DelCollections();
+	DelFullscreen();
 })
 function DelCollections() {
 	$titlediv = $('.collect-title'),
@@ -15,3 +16,13 @@ function DelCollections() {
 	$ksdiv.attr("style", "visibility: visible;");
 	$('.collect-title').removeAttr("style");
 }
+function DelFullscreen() {
+	$("#J_playerLrc").css("font-size", "");
+	$("#J_playerCover").width("250px");
+	//$("#J_albumCover").width(albumCoverwidth[0]);
+	$("#J_lyricScrollWrap").css("width", "250px");
+	//$(".ui-lrc-line").css("line-height", "");
+	$("#J_lrcWrap").css("padding-right", "5px");
+	$("#J_lrcWrap").css("width", "250px");
+	$("#J_lrcWrap").not("#J_playerCover a").unbind('click');
+	}
--- Collections.js
+++ Collections.js
@@ -2,6 +2,7 @@
 
 $(function () {
 	Collections();
+	fullscreenLyrics();
 })
 function Collections() {
 	jQuery(function ($) {
@@ -33,4 +34,45 @@ function Collections() {
 			$('.ui-album-sale').show();
 		});
 	});
+}
+
+function fullscreenLyrics() {
+	var middlewidth = $("#middle").width();
+	var lrcWrapwidth = lrcWrapright = albumCoverwidth = playerCoverwidth = lyricScrollWrapwidth = [];
+	lrcWrapwidth.push($("#J_lrcWrap").width());
+	lrcWrapright.push($("#J_lrcWrap").css("padding-right"));
+	lyricScrollWrapwidth.push($("#J_lyricScrollWrap").width());
+	albumCoverwidth.push($("#J_albumCover").width());
+	playerCoverwidth.push($("#J_playerCover").width());
+	$("#J_lyricScrollWrap").click(function () {
+		if ($("#J_tab").css("display") != "none" && $(".main-sidebar").css("display") != "none") {
+			$("#J_tab").hide("fast", function () {
+				$(".main-sidebar").hide("fast", function () {
+					$("#J_lrcWrap").css("padding-right", "0");
+					$("#J_lrcWrap").width(middlewidth);
+					//$("#J_albumCover").width("100%");
+					$("#J_playerCover").width("1%");
+					$("#J_lyricScrollWrap").css("width", "100%");
+					$("#J_playerLrc").css("font-size", "120%");
+					//$(".ui-lrc-line").css("line-height", "30px");
+				});
+			});
+		} else {
+			$("#J_playerLrc").css("font-size", "");
+			$("#J_playerCover").width(playerCoverwidth[0]);
+			//$("#J_albumCover").width(albumCoverwidth[0]);
+			$("#J_lyricScrollWrap").css("width", "350px");
+			//$(".ui-lrc-line").css("line-height", "");
+			$("#J_lrcWrap").css("padding-right", "20px");
+			$("#J_lrcWrap").animate({
+				width : "350px"
+			},
+				function () {
+				$(".main-sidebar").show("fast", function () {
+					$("#J_tab").show("fast");
+				});
+			})
+		}
+	})
+
 }
